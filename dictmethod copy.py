5f0187b1c1eca94bab2acf42e1157ed010fc0e70
Lars Arienti <lars.arienti@gmail.com>
2020-05-04 20:36:26 -0400

    flaskjawns

--- dictmethod copy.py
+++ dictmethod copy.py
@@ -1,41 +1,42 @@
 import pandas as pd
-reader=pd.read_csv("/Users/lars/Documents/Github/project2lars/merged_data.csv")
-
-for index, row in reader.iterrows():
-    d=row.to_dict()
-    print(d)
-
-
-
 from flask import Flask, request, jsonify
 import os 
+import csv
+
+file = '/Users/lars/Documents/Github/project2lars/merged_data.csv'
+with open(file) as fh:
+    rd = csv.DictReader(fh, delimiter=',')
+    dictionary = []
+    for row in rd:
+        dictionary.append(row)
+        
+reader=pd.read_csv("/Users/lars/Documents/Github/project2lars/merged_data.csv")
+
+dict_row={}
+movies_dict={}
+for index, row in reader.iterrows():
+    d=row.to_dict()
+    dict_row={d['title']:d}
+    movies_dict.update(dict_row)
+
 
 app = Flask(__name__)
 
-@app.route("/". methods=["GET"])
-def
+@app.route("/", methods=["GET"])
+def index():
+    return jsonify(movies_dict)
 
 @app.route("/movies/", methods=["GET"])
 def movies():
-    session = Session(engine)
-
-    entries=session.query(Movie.title, Movie.director).all()
-    
-    session.close()
-    all_entries = []
-    for m in entries:
-        movies_dict = {m.title:{m.director:r.release_year}}
-        all_entries.append(movies_dict)
-
-    return jsonify(all_entries)
+    movies_list=[]
+    for index, row in reader.iterrows():
+        d=row.to_dict()
+        movies_list.append(d['title'])
+    return jsonify(movies_list)
 
 @app.route("/movies/<moviename>/", methods=["GET"])
 def movie_name(moviename):
-    session = Session(engine)
-
-    session.close()
-    return jsonify(session.query(Movie.title, Movie.director, Movie.release_year).filter(Movie.title == moviename).all())
-        #add other fields 
+    return jsonify(movies_dict[moviename])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
